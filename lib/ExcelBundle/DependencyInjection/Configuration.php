f8ddfb610b7daf7bf125b767d7856239bb88909f
Nono1971 <herobrun@gmail.com>
2019-06-24 22:18:31 +0200

    fix DI deprecation

--- lib/ExcelBundle/DependencyInjection/Configuration.php
+++ lib/ExcelBundle/DependencyInjection/Configuration.php
@@ -17,8 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('onurb_excel');
+        $treeBuilder = new TreeBuilder('onurb_excel');
 
         return $treeBuilder;
     }
